abe2a1ffd52fe894310dc2b8af0019ce59e35b41
Guilherme-Augusto <guilhermex12g@gmail.com>
2025-02-10 11:12:25 -0300

    Update B.cs

--- B.cs
+++ B.cs
@@ -14,15 +14,22 @@ class Program
     	
     	for (int i = 0; i < 10; i++) {
     		for (int j = 0; j < 10; j++) {
-    			Console.Write("{0}º elemnto:",i+1);
+    			Console.Write("elemento {0}, {1}:",i+1,j+1);
     			matriz[i, j] = int.Parse(Console.ReadLine());
     	    }
     	}
-    	Console.WriteLine("Elementos na diagonal principal");
+    	Console.WriteLine("\nA matriz:");
+    	for (int i = 0; i < 10; i++) {
+    		for (int j = 0; j < 10; j++) {
+    		Console.Write(matriz[i, j] + " ");
+    		}
+    		Console.WriteLine();
+    	}	
+    	Console.WriteLine("\nElemntos na diagonal principal");
     	for (int i = 0; i < 10; i++) {
     		Console.Write(matriz[i,i] + " ");
-    	}	
-    	Console.WriteLine();
+    	}
+    Console.WriteLine();
     Console.WriteLine("Presione algo para sair . . . ");
 	Console.ReadKey(true);
     }
